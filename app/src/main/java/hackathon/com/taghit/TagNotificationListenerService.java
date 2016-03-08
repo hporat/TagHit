64d86032bbf348befa0888eb127cb68364e9bd37
gseffy <gseffy@gmail.com>
2016-03-08 09:20:23 +0200

    UPDATE

--- app/src/main/java/hackathon/com/taghit/TagNotificationListenerService.java
+++ app/src/main/java/hackathon/com/taghit/TagNotificationListenerService.java
@@ -1,6 +1,9 @@
 package hackathon.com.taghit;
 
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,6 +12,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
@@ -89,6 +93,30 @@ public class TagNotificationListenerService extends NotificationListenerService 
 
                 if(isImportantMessage){
                     // todo - create notification
+                    /*NotificationCompat.Builder mBuilder =
+                            new NotificationCompat.Builder(this)
+                                    .setSmallIcon(R.drawable.notification_icon)
+                                    .setContentTitle("My notification")
+                                    .setContentText("Hello World!");
+                    // Creates an explicit intent for an Activity in your app
+                    Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.whatsapp");
+                    // The stack builder object will contain an artificial back stack for the
+                    // started Activity.
+                    // This ensures that navigating backward from the Activity leads out of
+                    // your application to the Home screen.
+                    TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+                    // Adds the back stack for the Intent (but not the Intent itself)
+                    stackBuilder.addParentStack(MainActivity.class);
+                    // Adds the Intent that starts the Activity to the top of the stack
+                    stackBuilder.addNextIntent(launchIntent);
+                    PendingIntent resultPendingIntent =
+                            stackBuilder.getPendingIntent(
+                                    0,
+                                    PendingIntent.FLAG_UPDATE_CURRENT
+                            );
+                    mBuilder.setContentIntent(resultPendingIntent);
+                    NotificationManager mNotificationManager =
+                            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);*/
                 }
             }
         }
