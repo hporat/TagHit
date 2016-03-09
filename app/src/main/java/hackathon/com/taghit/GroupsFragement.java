5f58d7044c60c3485a1c0989f1774ec8e2cf2977
SofiaPoliakov <sofia200975@gmail.com>
2016-03-09 09:21:42 +0200

    compile fix

--- app/src/main/java/hackathon/com/taghit/GroupsFragement.java
+++ app/src/main/java/hackathon/com/taghit/GroupsFragement.java
@@ -1,17 +1,21 @@
 package hackathon.com.taghit;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.widget.AppCompatTextView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.app.Activity;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.util.*;
@@ -23,10 +27,11 @@ import hackathon.com.taghit.model.GroupsTags;
  * A simple {@link Fragment} subclass.
  */
 public class GroupsFragement extends BaseFragment implements
-        AdapterView.OnItemClickListener {
+        AdapterView.OnItemClickListener, View.OnClickListener {
 
-    ArrayAdapter<String> adapter;
-    List<String> mGroups;
+    ArrayAdapter<String> mGroupsListAdapter;
+    List<String> mGroupsList;
+    EditText mNewGroupTxt;
 
     public GroupsFragement() {
         // Required empty public constructor
@@ -41,21 +46,24 @@ public class GroupsFragement extends BaseFragment implements
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragmen
 
-        View view = inflater.inflate(R.layout.groups_fragement, container, false);
+        View returnView = inflater.inflate(R.layout.groups_fragement, container, false);
 
-        ListView lstItems = (ListView)view.findViewById(R.id.listGrpView);
-        mGroups = GroupsTags.getGroups();
-        adapter = new GroupsListAdapter(getActivity(), mGroups);
-        lstItems.setAdapter(adapter);
+        ListView lstItems = (ListView)returnView.findViewById(R.id.listGrpView);
+        mGroupsList = GroupsTags.getGroups();
+        mGroupsListAdapter = new GroupsListAdapter(getActivity(), mGroupsList);
+        lstItems.setAdapter(mGroupsListAdapter);
         lstItems.setOnItemClickListener(this);
 
-        return view;
+        FloatingActionButton addButton = (FloatingActionButton)returnView.findViewById(R.id.addGroupButton);
+        addButton.setOnClickListener(this);
+
+        return returnView;
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         //String selectedGrpName = ((AppCompatTextView) view).getText().toString();
-        String selectedGrpName = mGroups.get(+position);
+        String selectedGrpName = mGroupsList.get(+position);
         System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!" + selectedGrpName);
 
         if(mListener != null) {
@@ -66,6 +74,11 @@ public class GroupsFragement extends BaseFragment implements
 
     public static GroupsFragement newInstance( BaseFragment.PageFragmentListener listener) {
         return new GroupsFragement(listener);
+    }
+
+    @Override
+    public void onClick(View v) {
+        initiatePopupWindow();
     }
 
     public class GroupsListAdapter extends ArrayAdapter<String>{
@@ -86,8 +99,39 @@ public class GroupsFragement extends BaseFragment implements
             ImageView imageView = (ImageView) rowView.findViewById(R.id.groupImg);
             txtTitle.setText(mGroupNames.get(position));
 
-            imageView.setImageResource(R.drawable.group1);
+            imageView.setImageResource(R.drawable.group2);
             return rowView;
         }
     }
+
+    private PopupWindow pw;
+    private void initiatePopupWindow() {
+        try {
+            //We need to get the instance of the LayoutInflater, use the context of this activity
+            LayoutInflater inflater = (LayoutInflater)getActivity()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            //Inflate the view from a predefined XML layout
+            View layout = inflater.inflate(R.layout.add_group_popup,
+                    (ViewGroup)getActivity().findViewById(R.id.popup_add_group));
+            // create a 300px width and 470px height PopupWindow
+            pw = new PopupWindow(layout, 500, 370, true);
+            // display the popup in the center
+            pw.showAtLocation(layout, Gravity.CENTER, 0, 0);
+
+            mNewGroupTxt = (EditText) layout.findViewById(R.id.new_group_text);
+            Button addButton = (Button) layout.findViewById(R.id.add_group_button_popup);
+            addButton.setOnClickListener(add_button_click_listener);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private View.OnClickListener add_button_click_listener = new View.OnClickListener() {
+        public void onClick(View v) {
+            GroupsTags.addGroup(mNewGroupTxt.getText().toString());
+            pw.dismiss();
+        }
+    };
+
 }
